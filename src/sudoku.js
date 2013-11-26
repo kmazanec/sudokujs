938912fbf4ad907c95d2377326ee5e3ba067c0d0
kmazanec <k.mazanec@gmail.com>
2013-11-26 12:07:29 -0600

    write code and spec for parsing initial board string into 2d array

--- src/sudoku.js
+++ src/sudoku.js
@@ -8,13 +8,34 @@ var Board = {
   init : function(board_string){
     this.board = board_string;
   },
+
+  // Solve function and it's helpers
+
   solve : function(){
 
   },
+
+
+  // Convert to 2d array function and it's helpers
+
   parse : function(){
-    this.board = this.board.split();
-    this.board.forEach(item){
-      
+    this.convertToArray();
+    this.convertToInt();
+    this.splitRows();
+  },
+  convertToArray : function(){
+    this.board = this.board.split("");
+  },
+  convertToInt : function(){
+    this.board = _.map(this.board, function(char){
+      return parseInt(char, 10);
+    });
+  },
+  splitRows : function(){
+    var splitBoard = [];
+    for (var i = 9; i <= 81; i += 9) {
+      splitBoard.push(this.board.slice(i-9, i));
     }
+    this.board = splitBoard;
   }
 };
